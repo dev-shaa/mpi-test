19a3fe87d7d4ea701463e677d08bb9ede7fe9134
Salvatore Di Gennaro <salvatore.digennaro@outlook.it>
2025-06-25 10:58:20 +0200

    Add SLURM script

--- main.c
+++ main.c
@@ -16,6 +16,9 @@ int main(int argc, char **argv)
     MPI_Dims_create(size, 2, dims);
     MPI_Cart_create(MPI_COMM_WORLD, 2, dims, periods, 0, &grid_comm);
 
+    if (rank == 0)
+        printf("Grid: %d %d\n", dims[0], dims[1]);
+
     MPI_Finalize();
 
     return 0;
